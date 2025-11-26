e4bcac5beb0439143d93e4a1656b6064a6409132
Abinauw B <reachabinauwbalaji@gmail.com>
2025-11-26 19:59:41 +0530

    Updated RightPrymid.java

--- RightPrymid.java
+++ RightPrymid.java
@@ -12,10 +12,11 @@ public class RightPrymid{
             for (int j = 1;j<=i;j++){
                 System.out.print("*");
             }
-            System.out.println();
+             System.out.println();
         }
         sc.close();
     }
 }
 
     
+
